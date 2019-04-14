a4e4097a66c20d18041a3263660bcc3660f85eac
Edwin Chen <edwinchen@auguried.com>
2019-04-15 07:46:16 +0800

    Render spinner and load more button

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -98,9 +98,11 @@ class Home extends Component {
                       />
               })}
             </FourColGrid>
+            {this.state.loading ? <Spinner /> : null}
+            {(this.state.currentPage <= this.state.totalPages && !this.state.loading) ?
+              <LoadMoreBtn text="Load More" onClick={this.loadMoreItems} /> :
+              null }
           </div>
-          <Spinner />
-          <LoadMoreBtn />
       </div>
     )
   }
