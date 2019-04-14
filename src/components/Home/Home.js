00d656167f89f71fa30dc1cb89f6998f54751b3b
Edwin Chen <edwinchen@auguried.com>
2019-04-15 07:38:31 +0800

    Render movies in FourColGrid component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -83,7 +83,22 @@ class Home extends Component {
             />
             <SearchBar callback={this.searchItems} />
           </div> : null }
-          <FourColGrid />
+          <div className="rmdb-home-grid">
+            <FourColGrid
+              header={this.state.searchTerm ? 'Search Result' : 'Popular Movies'}
+              loading={this.state.loading}
+            >
+              {this.state.movies.map((element, i) => {
+                return <MovieThumb
+                        key={i}
+                        clickable={true}
+                        image={element.poster_path ? `${IMAGE_BASE_URL}${POSTER_SIZE}${element.poster_path}` : './images/no_image.jpg'}
+                        imageId={element.id}
+                        movieName={element.original_title}
+                      />
+              })}
+            </FourColGrid>
+          </div>
           <Spinner />
           <LoadMoreBtn />
       </div>
