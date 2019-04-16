826a5e42f87ee86316a257dc2a36eb9b89a4602a
Edwin Chen <edwinchen@auguried.com>
2019-04-16 21:54:58 +0800

    Set up BrowserRouter in root component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from '../elements/Header/Header';
 import Home from '../Home/Home';
+import Movie from '../Movie/Movie';
+import NotFound from '../elements/NotFound/NotFound';
 
 const App = () => {
   return (
-    <div>
-      <Header />
-      <Home />
-    </div>
+    <BrowserRouter>
+      <React.Fragment>
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/:movieId" component={Movie} />
+          <Route component={NotFound} />
+        </Switch>
+      </React.Fragment>
+    </BrowserRouter>
   )
 }
 
